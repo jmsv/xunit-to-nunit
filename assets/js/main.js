2cae5eb99a10efc74543e3e166f57bbdb44238fb
jamesevickery <jamesevickery.dev@gmail.com>
2017-06-23 13:06:19 +0100

    Convert to proper NUnit syntax
    Instead of Assert.That and Is.Condition, conversions now use direct alternatives, e.g. Assert.Equal -> Assert.AreEqual. Also refactored a lot of existing code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,79 @@
 // Assert calls to be converted are listed here
 var assertsList = [
     {
-        AssertType: 'Equal',
-        Criteria: 'Is.EqualTo(',
-        Comparison: true,
+        XUnitAssert: 'Equal',
+        NUnitAssert: 'AreEqual'
     },
     {
-        AssertType: 'NotEmpty',
-        Criteria: 'Is.Not.Empty',
-        Comparison: false,
+        XUnitAssert: 'NotEqual',
+        NUnitAssert: 'AreNotEqual'
     },
     {
-        AssertType: 'NotEqual',
-        Criteria: 'Is.Not.EqualTo(',
-        Comparison: true,
+        XUnitAssert: 'Same',
+        NUnitAssert: 'AreSame'
     },
     {
-        AssertType: 'Contains',
-        Criteria: 'Does.Contain(',
-        Comparison: true,
+        XUnitAssert: 'NotSame',
+        NUnitAssert: 'AreNotSame'
     },
+    {
+        XUnitAssert: 'Empty',
+        NUnitAssert: 'IsEmpty'
+    },
+    {
+        XUnitAssert: 'NotEmpty',
+        NUnitAssert: 'IsNotEmpty'
+    },
+    {
+        XUnitAssert: 'IsType',
+        NUnitAssert: 'IsInstanceOfType'
+    },
+    {
+        XUnitAssert: 'IsNotType',
+        NUnitAssert: 'IsNotInstanceOfType'
+    },
+    {
+        XUnitAssert: 'Null',
+        NUnitAssert: 'IsNull'
+    },
+    {
+        XUnitAssert: 'NotNull',
+        NUnitAssert: 'IsNotNull'
+    },
+    {
+        XUnitAssert: 'True',
+        NUnitAssert: 'IsTrue'
+    },
+    {
+        XUnitAssert: 'False',
+        NUnitAssert: 'IsFalse'
+    }
 ];
+
+
+// Other differences between XUnit and NUnit that should be converted
+var otherSyntaxDifferenceList = [
+    {
+        XUnitSyntax: 'using Xunit;',
+        NUnitSyntax: 'using NUnit.Framework;'
+    },
+    {
+        XUnitSyntax: '[Fact]',
+        NUnitSyntax: '[Test]'
+    },
+    {
+        XUnitSyntax: '[Theory]',
+        NUnitSyntax: '[Test]'
+    },
+    {
+        XUnitSyntax: 'InlineData(',
+        NUnitSyntax: 'TestCase('
+    },
+    {
+        XUnitSyntax: 'ClassData(',
+        NUnitSyntax: 'TestCaseSource('
+    },
+]
 
 
 // This test is loaded into the XUnit paste box on page load
@@ -37,7 +90,6 @@ public class SomeTests
         Assert.NotEmpty(testString);
         Assert.Equal("avocado", testString);
         Assert.NotEqual("potato", testString);
-        Assert.Contains("avo", testString);
     }
  
     [Theory]
@@ -53,118 +105,12 @@ public class SomeTests
 `;
 
 
-// Gives strings the 'insertAt' method for inserting substring into string at index
-String.prototype.insertAt = function (index, string) {
-    return this.substr(0, index) + string + this.substr(index);
-}
-
-
-// Get index of the end of a parameter based on it's start index
-// Skips over brackets and quotes to reach the end of the parameter
-function getEndOfParamIndex(line, startOfParamIndex) {
-    if (line == '/\S/') return '';
-    var countParenthesis = 0;
-    var countBracket = 0;
-    var inQuote = false;
-    var inDoubleQuote = false;
-    for (var i = startOfParamIndex; i <= line.length; i++) {
-        if (line[i] == '(') countParenthesis += 1;
-        if (line[i] == ')') countParenthesis -= 1;
-        if (line[i] == '[') countBracket += 1;
-        if (line[i] == ']') countBracket -= 1;
-        if (line[i] == '\'') inQuote = !inQuote;
-        if (line[i] == '"') inDoubleQuote = !inDoubleQuote;
-        if (line[i] == ';') {
-            return i - 1;
-        }
-        if (line[i] == ',') {
-            if (countParenthesis == 0 &
-                countBracket == 0 &
-                !inQuote &
-                !inDoubleQuote) {
-                return i;
-            }
-        }
-    }
-    return line.length;
-}
-
-
-// Get index of string where Assert.That criteria should be inserted
-function getCriteriaIndex(line, startIndex) {
-    getEndOfParamIndex(line, startIndex);
-}
-
-
-// Gets start index of a parameter based on the ending index of the first parameter
-function getNextParamStartIndex(line, endOfFirstParamIndex) {
-    for (var i = endOfFirstParamIndex; i < line.length; i++) {
-        if (!(line[i] == ',' | line[i] == '/\S/')) {
-            return i;
-        }
-    }
-}
-
-
-// Swaps two Assert params, e.g. 'Assert.Equal("one", getOne())' to 'Assert.Equal(getOne(), "one")'
-// Necassary because XUnit and NUnit's 'expected' and 'actual' parameters are reversed
-function swapTwoAssertParams(line, openBracketIndex) {
-    if (line[openBracketIndex] != '(') {
-        return 'invalid openBracketIndex supplied';
-    }
-    var firstParamEndIndex = getEndOfParamIndex(line, openBracketIndex + 1);
-    var paramFirst = line.substring(openBracketIndex + 1, firstParamEndIndex);
-    var secondParamStartIndex = getNextParamStartIndex(line, firstParamEndIndex + 1);
-    var secondParamEndIndex = getEndOfParamIndex(line, getEndOfParamIndex(line, secondParamStartIndex));
-    var paramSecond = line.substring(secondParamStartIndex + 1, secondParamEndIndex);
-    return line.substring(0, openBracketIndex) + '(' + paramSecond + ', ' + paramFirst + ');';
-}
-
-
-// Where to put criteria string for non-comparison Asserts
-function getStandaloneCriteriaIndex(line) {
-    for (var i = line.length - 1; i > 0; i--) {
-        if (!(line[i] == ')' | line[i] == ';')) {
-            return i + 1;
-        }
-    }
-}
-
-
-// Converts Asserts for a single line for a single assert
-function convertSingleAssert(line, assert) {
-    var assertCallName = 'Assert.' + assert.AssertType;
-    var index = line.indexOf(assertCallName);
-    if (index < 0) {
-        return line;
-    }
-    var bracketIndex = index + assertCallName.length;
-
-    if (assert.Comparison) {
-        line = swapTwoAssertParams(line, bracketIndex);
-        var criteriaIndex = getEndOfParamIndex(line, bracketIndex + 1) + 1;
-        line = line.insertAt(criteriaIndex + 1, assert.Criteria);
-    } else {
-        var insertIndex = getStandaloneCriteriaIndex(line)
-        line = line.insertAt(insertIndex, ', ' + assert.Criteria);
-    }
-
-    if (assert.Criteria[assert.Criteria.length - 1] == '(') {
-        if (line[line.length - 1] == ';') {
-            line = line.insertAt(line.length - 1, ')');
-        } else {
-            line += ')';
-        }
-    }
-    line = line.replace(assert.AssertType, 'That');
-    return line;
-}
-
-
 // Converts Assert statements for each Assert type in assertsList (line 1)
 function convertLineAssert(line) {
     for (var i = 0; i < assertsList.length; i++) {
-        line = convertSingleAssert(line, assertsList[i])
+        var x = 'Assert.' + assertsList[i].XUnitAssert + '(';
+        var n = 'Assert.' + assertsList[i].NUnitAssert + '(';
+        line = line.replace(x, n);
     }
     return line;
 }
@@ -181,39 +127,47 @@ function addTestFixtureLine(line) {
 }
 
 
-// Convert an individual line
+// Remove line from list of lines if matches search string
+function removeLineFromList(linesList, searchString) {
+    for (var i = linesList.length - 1; i--;) {
+        if (linesList[i] === searchString) linesList.splice(i, 1);
+    }
+    return linesList
+}
+
+
+// Convert line from XUnit syntax to NUnit syntax, including Assert statements
 function convertLine(line) {
-    line = line.replace("using Xunit;", "using NUnit.Framework;");
     line = convertLineAssert(line);
-    line = line.replace("using Xunit;", "using NUnit.Framework;");
-    line = line.replace('[Fact]', '[Test]');
-    line = line.replace('[Theory]', '[Test]');
-    line = line.replace('[InlineData', '[TestCase');
-    line = line.replace('[ClassData', '[TestCaseSource');
     line = addTestFixtureLine(line);
+    
+    for (var i = 0; i < otherSyntaxDifferenceList.length; i++) {
+        var x = otherSyntaxDifferenceList[i].XUnitSyntax;
+        var n = otherSyntaxDifferenceList[i].NUnitSyntax;
+        line = line.replace(x, n);
+    }
     return line;
 }
 
 
 // Main method to convert code
 function convertCode(codeIn) {
+    // Split code into list, breaking at newlines
     var codeLines = codeIn.split('\n');
 
     for (var i = 0; i < codeLines.length; i++) {
         codeLines[i] = convertLine(codeLines[i]);
     }
 
-    for (var i = codeLines.length - 1; i--;) {
-        if (codeLines[i] === 'using Xunit.Abstractions;') {
-            codeLines.splice(i, 1);
-        }
-    }
+    codeLines = removeLineFromList(codeLines, 'using Xunit.Abstractions;');
 
+    // Join list of lines to form newline seperated string
     var codeOut = codeLines.join('\n');
     return codeOut + '\n';
 }
 
 
+// Display message when converted test has been copied
 function showCopiedSnackbar() {
     var x = document.getElementById("snackbar")
     x.className = "show";
@@ -223,12 +177,15 @@ function showCopiedSnackbar() {
 }
 
 
+// Function to run when page loads
 function loadPage() {
+    // Initialise clipboard
     new Clipboard('.clipboard');
 }
-window.onload = loadPage;
+window.onload = loadPage; // Runs above function
 
 
+// Set up Angular module, with controller
 angular.module('XUnitToNUnit', [])
     .controller('ConverterController', function ($scope) {
         var ConverterController = this;
